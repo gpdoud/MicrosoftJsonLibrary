bd807f0d0f69dc8b3240d46eb806ba1eb26bd40a
Greg Doud <gpdoud@gmail.com>
2019-10-04 07:41:08 -0400

    Add project files.

---
+++ MicroosoftJson/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace MicrosoftJson {
+
+
+    ///<summary>
+    /// One of the differences between Newtonsoft.Json and System.Text.Json
+    /// is that Microsoft's library does not operate on fields like Customer.Active.
+    /// It only operates on properties like Id, Name, and Sales. When run the 
+    /// serialize does not output the Active field value.
+    ///</summary>
+    class Customer {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Sales { get; set; }
+        public bool Active = true; // Microsoft's JSON lib doesn't serialize fields like this
+
+        public string Serialize() {
+            return JsonSerializer.Serialize(this, typeof(Customer),
+                                            new JsonSerializerOptions { WriteIndented = true });
+        }
+        public static Customer Deserialize(string json) {
+            return JsonSerializer.Deserialize(json, typeof(Customer)) as Customer;
+        }
+        public override string ToString() {
+            return $"Customer: id[{Id}],name[{Name}],sales[{Sales}]";
+        }
+    }
+}
---
+++ MicroosoftJson/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MicrosoftJson {
+    class Program {
+
+        void Run() {
+            SerializeDeserializeExample();
+            JsonReaderExample();
+            JsonDocumentExample();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Serialize turns a class instance into JSON
+        /// Deserialize turns JSON into a class instance
+        /// 
+        /// This is done automatically with WebAPI controller methods
+        /// </summary>
+        void SerializeDeserializeExample() {
+            var cust = new Customer { Id = 1, Name = "Amazon", Sales = 1000000, Active = true };
+            Console.WriteLine(cust.Serialize());
+
+            var cust2json = "{ \"Id\" : 2, \"Name\" : \"WS\", \"Sales\" : 1000000 }";
+            var cust2 = Customer.Deserialize(cust2json.ToString());
+            Console.WriteLine(cust2.Serialize());
+        }
+        /// <summary>
+        /// The JsonReader is a forward-only, sequential reader. It just reads everything
+        /// inside the JSON file.
+        /// </summary>
+        void JsonReaderExample() {
+
+            var appJson = "{ \"AppProperties\" : { \"ConnectionString\": \"server=localhost:12345;database=AppDb;trusted_connection=true;\" }}";
+            byte[] data = Encoding.UTF8.GetBytes(appJson.ToArray());
+            Utf8JsonReader reader = new Utf8JsonReader(data, isFinalBlock: true, state: default);
+            while(reader.Read()) {
+                Console.WriteLine(reader.TokenType);
+                switch(reader.TokenType) {
+                    case JsonTokenType.PropertyName:
+                    case JsonTokenType.String:
+                        var text = reader.GetString();
+                        Console.Write($" {text}");
+                        break;
+                    case JsonTokenType.Number:
+                        var nbr = reader.GetInt32();
+                        Console.Write($" {nbr}");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// The JsonDocument allows processing of all or part of a JSON file.
+        /// This example shows getting a connection string from a config file
+        /// in JSON format that looks like this:
+        /// 
+        /// {
+        ///     AppProperties: {
+        ///         ConnectionString: "server=localhost:12345;database=AppDb;trusted_connection=true"
+        ///     }
+        /// }
+        /// </summary>
+        void JsonDocumentExample() {
+
+            var appJson = "{ \"AppProperties\" : { \"ConnectionString\": \"server=localhost:12345;database=AppDb;trusted_connection=true;\" }}";
+            using(JsonDocument doc = JsonDocument.Parse(appJson)) {
+                var root = doc.RootElement;
+                var appProp = root.GetProperty("AppProperties");
+                var connStr = appProp.GetProperty("ConnectionString").GetString();
+                Console.WriteLine($"connStr is [{connStr}]");
+            }
+            
+        }
+        static void Main(string[] args) {
+            (new Program()).Run();
+        }
+    }
+}
